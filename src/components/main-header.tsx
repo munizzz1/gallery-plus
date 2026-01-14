3021356f315430d1fb630636d0801bdbcba6803e
Jhonatan Muniz <jhonatanmuniz123@gmail.com>
2026-01-14 12:09:19 -0300

    feat: create main-header

---
+++ src/components/main-header.tsx
@@ -0,0 +1,27 @@
+import Container from "./container";
+import { Link } from "react-router";
+import cx from "classnames";
+
+import Button from "./button";
+import Logo from "../assets/images/galeria-plus-full-logo.svg?react";
+
+interface MainHeaderProps extends React.ComponentProps<"div"> {}
+
+export default function MainHeader({ className, ...props }: MainHeaderProps) {
+  return (
+    <Container
+      as="header"
+      className={cx("flex justify-between items-center gap-10", className)}
+      {...props}
+    >
+      <Link to="/">
+        <Logo className="h-5" />
+      </Link>
+
+      <div className="flex items-center gap-3">
+        <Button>Nova foto</Button>
+        <Button variant="secondary">Criar álbum</Button>
+      </div>
+    </Container>
+  );
+}
