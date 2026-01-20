880768464e429a39b387515e8059fa9e255f115c
Jhonatan Muniz <jhonatanmuniz123@gmail.com>
2026-01-20 16:54:32 -0300

    feat: create photos-list component

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,14 +1,12 @@
-import type { Photo } from "../contexts/photos/models/photo";
-
-import PhotoWidget from "../contexts/photos/components/photo-widget";
 import Container from "../components/container";
+import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-9">
-        <PhotoWidget
-          photo={{
+      <PhotosList
+        photos={[
+          {
             id: "123",
             title: "Test",
             imageId: "portrait-tower.png",
@@ -17,24 +15,9 @@ export default function PageHome() {
               { id: "1234", title: "Album 2" },
               { id: "1235", title: "Album 3" },
             ],
-          }}
-        />
-
-        <PhotoWidget
-          photo={{
-            id: "123",
-            title: "Test",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "123", title: "Album 1" },
-              { id: "1234", title: "Album 2" },
-              { id: "1235", title: "Album 3" },
-            ],
-          }}
-        />
-
-        <PhotoWidget photo={{} as Photo} loading />
-      </div>
+          },
+        ]}
+      />
     </Container>
   );
 }
