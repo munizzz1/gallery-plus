41414dd1572e29dc65534473b36b272298d194c9
Jhonatan Muniz <jhonatanmuniz123@gmail.com>
2026-01-20 17:32:03 -0300

    feat: create album-filter component

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,18 @@
-import Container from "../components/container";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
 import PhotosList from "../contexts/photos/components/photos-list";
+import Container from "../components/container";
 
 export default function PageHome() {
   return (
     <Container>
+      <AlbumsFilter
+        albums={[
+          { id: "123", title: "Album 1" },
+          { id: "1234", title: "Album 2" },
+          { id: "1235", title: "Album 3" },
+        ]}
+        className="mb-9"
+      />
       <PhotosList
         photos={[
           {
